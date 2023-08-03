0e0b429057c051854a19ab71cd07b32d6c0ca5fb
PriyanshuJain13 <pjstlyes13@gmail.com>
2023-08-03 20:26:05 +0530

    completed logo section

--- components/HomeBody.jsx
+++ components/HomeBody.jsx
@@ -5,17 +5,21 @@ import resume from "@assets/resume.png";
 import button from "@assets/button.png";
 import laptop from "@assets/laptop.png";
 import Image from "next/image";
-import leetcode from '@assets/icons/leetcode.svg'
-import github from '@assets/icons/github.svg'
-import codeforces from '@assets/icons/codeforces.svg'
-import linkedIn from '@assets/icons/linkedin.svg'
+import leetcode from "@assets/icons/leetcode.svg";
+import github from "@assets/icons/github.svg";
+import codeforces from "@assets/icons/codeforces.svg";
+import linkedIn from "@assets/icons/linkedin.svg";
 
 const HomeBody = () => {
   return (
     <div className="text-white my-14 flex flex-wrap md:m-">
       <div className="blackBox space-x-2 flex flex-row w-full h-80 p-8 md:w-2/5">
         <div className="mypic w-1/2 pt-10">
-          <Image src={myPic} className="rounded-tl-2xl rounded-br-2xl" />
+          <Image
+            src={myPic}
+            alt="myPic"
+            className="rounded-tl-2xl rounded-br-2xl"
+          />
         </div>
         <div className="pt-10 w-1/2 space-y-1 sm:space-y-2">
           <p className="greyTxt">A web developer</p>
@@ -34,7 +38,11 @@ const HomeBody = () => {
         <div className="flex flex-row mt-10 space-x-2">
           <div className="blackBox w-1/2 h-60 p-4 space-y-2">
             <div className="flex justify-center">
-              <Image src={credentials} className="w-[160px] lg:w-[250px]" />
+              <Image
+                src={credentials}
+                alt="cred"
+                className="w-[160px] lg:w-[250px]"
+              />
             </div>
             <div className="flex justify-between lg:px-8">
               <div className="">
@@ -42,14 +50,18 @@ const HomeBody = () => {
                 <p className="whiteTxt">Credentials</p>
               </div>
               <button src="/">
-                <Image src={button} className="w-10 glow-btn" />
+                <Image src={button} alt="glow" className="w-10 glow-btn" />
               </button>
             </div>
           </div>
 
           <div className="blackBox w-1/2 h-60 p-4 space-y-2">
             <div className="flex justify-center">
-              <Image src={laptop} className="w-[100px] xl:w-[150px]" />
+              <Image
+                src={laptop}
+                alt="laptop"
+                className="w-[100px] xl:w-[150px]"
+              />
             </div>
             <div className="flex justify-between lg:px-8">
               <div className="">
@@ -57,7 +69,7 @@ const HomeBody = () => {
                 <p className="whiteTxt">Credentials</p>
               </div>
               <button src="/">
-                <Image src={button} className="w-10 glow-btn" />
+                <Image src={button} alt="cred" className="w-10 glow-btn" />
               </button>
             </div>
           </div>
@@ -67,7 +79,11 @@ const HomeBody = () => {
       <div className="w-full space-y-2 flex flex-row mt-8 flex-wrap sm:space-y-0 sm:justify-between">
         <div className="blackBox w-full sm:w-1/4 xl:w-1/5 h-60 p-4 space-y-6 lg:space-y-2">
           <div className="flex justify-center">
-            <Image src={resume} className="w-[100px] xl:w-[150px]" />
+            <Image
+              src={resume}
+              alt="resume"
+              className="w-[100px] xl:w-[150px]"
+            />
           </div>
           <div className="flex justify-evenly lg:px-8 lg:justify-between">
             <div className="">
@@ -76,18 +92,42 @@ const HomeBody = () => {
             </div>
             <button src="">
               <a href="/resume" target="_blank">
-                <Image src={button} className="w-10 glow-btn" />
+                <Image src={button} alt="button" className="w-10 glow-btn" />
               </a>
             </button>
           </div>
         </div>
 
         <div className="blackBox w-full sm:w-1/3 xl:w-1/2 h-60 p-4 space-y-6 lg:space-y-2">
-          <div className="flex h-2/3 space-x-4 xl:space-x-16">
-            <Image src={linkedIn} className="w-[40px] xl:w-[60px]" />
-            <Image src={github} className="w-[40px] xl:w-[60px]" />
-            <Image src={leetcode} className="w-[40px] xl:w-[60px]" />
-            <Image src={codeforces} className="w-[40px] xl:w-[60px]" />
+          <div className="flex justify-evenly h-2/3 space-x-4 xl:space-x-16">
+            <a href="https://www.linkedin.com/in/priyanshu-jain-637406214" className="flex flex-between" target="_blank">
+              <Image
+                src={linkedIn}
+                alt="logo"
+                className="w-[40px] xl:w-[60px] hoverLogo1"
+              />
+            </a>
+            <a href="https://github.com/PriyanshuJain13" className="flex flex-between" target="_blank">
+              <Image
+                src={github}
+                alt="logo"
+                className="w-[40px] xl:w-[60px] hoverLogo2"
+              />
+            </a>
+            <a href="https://leetcode.com/pjrise13/" className="flex flex-between" target="_blank">
+              <Image
+                src={leetcode}
+                alt="logo"
+                className="w-[40px] xl:w-[60px] hoverLogo3"
+              />
+            </a>
+            <a href="https://codeforces.com/profile/priyAnshu.13" className="flex flex-between" target="_blank">
+              <Image
+                src={codeforces}
+                alt="logo"
+                className="w-[40px] xl:w-[60px] hoverLogo4"
+              />
+            </a>
           </div>
           <div className="flex lg:px-8 lg:justify-between">
             <div className="">
