cc3a442ed83fd4ddcf34f71d6f3c148451876454
PriyanshuJain13 <pjstlyes13@gmail.com>
2023-08-07 20:15:23 +0530

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,16 @@ import Navbar from "@components/Navbar";
 import HomeBody from "@components/HomeBody";
 import Particle from "@components/Particle";
 import Contact from "@components/Contact";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-4 my-8 mx-6 md:mx-14">
+    <main className="flex min-h-screen flex-col pt-4 pl-4 pr-4 mt-4 mx-6 md:mx-14">
       <div className="z-10">
         <Navbar />
         <HomeBody />
         <Contact />
+        <Footer />
       </div>
       <Particle />
     </main>
---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <p className='text-white flex justify-center items-center p-6 mt-5 bg-black'>© All rights reserved  by Priyanshu Jain</p>
+    </div>
+  )
+}
+
+export default Footer
