1698f636c17f5eff477ecc9418954d2d3a749528
PriyanshuJain13 <pjstlyes13@gmail.com>
2023-09-06 22:49:38 +0530

    Edited about section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -54,20 +54,13 @@ const page = () => {
         </div>
         <div className="right text-white lg:w-1/2">
           <div id="about" className="mt-8 leading-8 text-slate-300 lg:mt-0">
-            Back in 2012, I decided to try my hand at creating custom Tumblr
-            themes and tumbled head first into the rabbit hole of coding and web
-            development. Fast-forward to today, and I've had the privilege of
-            building software for an advertising agency, a start-up, a
-            student-led design studio, and a huge corporation. My main focus
-            these days is building products and leading projects for our clients
-            at Upstatement. In my free time I've also released an online video
-            course that covers everything you need to know to build a web app
-            with the Spotify API. When I'm not at the computer, I'm usually rock
-            climbing, hanging out with my wife and two cats, or running around
-            Hyrule searching for Korok seeds
+            Back in 2021, I decided to try my hand at web
+            development. Currently I am a 3rd year student at JAYPEE INSTITUTE OF INFORMATION TECHNOLOGY and doing my Bachelors in Technology in field of Computer Science. Also Looking 👨‍🎓 for Software developer opputunities. I have created some projects and learned a lot about
+            development from them and also done programming in other languages like C++ , Python🐍 . When I'm not at the computer, I'm usually doing workout
+            or hanging out with my friends, or going through some books.
           </div>
           <div id="skills">
-
+            
           </div>
           <div id="projects"></div>
         </div>
