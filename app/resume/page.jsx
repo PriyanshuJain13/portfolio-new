84dda0241a42502a22f8a2bf7775530319820a29
PriyanshuJain13 <pjstlyes13@gmail.com>
2024-01-10 20:29:20 +0530

    change resume

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='text-white resumePage' >
-      <embed width="100%" height="100%" name="plugin" src="/resume.pdf" type="application/pdf"/>
+      <embed width="100%" height="100%" name="plugin" src="/resume1.pdf" type="application/pdf"/>
     </div>
   )
 }
